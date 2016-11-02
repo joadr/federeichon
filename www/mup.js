45626db43aac1ab48122c28bda7d5cb936cd27d0
diegovt21 <diegovt21@gmail.com>
2016-11-02 18:18:29 -0300

    Mup and react-native-meteor

---
+++ www/mup.js
@@ -0,0 +1,37 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '45.55.141.9',
+      username: 'root',
+      // pem:
+      password: 'federeichon2016'
+      // or leave blank for authenticate from ssh-agent
+    }
+  },
+
+  meteor: {
+    name: 'federeichon',
+    path: '.',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    env: {
+      ROOT_URL: 'http://45.55.141.9',
+      MONGO_URL: 'mongodb://localhost/federeichon'
+    },
+
+    dockerImage: 'abernix/meteord:base',
+    deployCheckWaitTime: 60
+  },
+
+  mongo: {
+    oplog: true,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
