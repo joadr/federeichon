b43d453227fa06e8e608dda42c42191ca69eee09
Joaquin Diaz <joaquin.diaz@ies.cl>
2016-11-08 02:46:38 -0300

    renamed app from iOS to Federeichon

--- app/android/app/src/main/java/com/federeichon/MainActivity.java
+++ app/android/app/src/main/java/com/federeichon/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ios;
+package com.federeichon;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "iOS";
+        return "Federeichon";
     }
 }
--- app/ios/FedereichonTests/FedereichonTests.m
+++ app/ios/FedereichonTests/FedereichonTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface iOSTests : XCTestCase
+@interface FedereichonTests : XCTestCase
 
 @end
 
-@implementation iOSTests
+@implementation FedereichonTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
