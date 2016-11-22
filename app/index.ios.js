62e600e5a9a2f5be516e2843cf7fecacff00a0df
Joaquin Diaz <joaquin.diaz@ies.cl>
2016-11-22 13:45:08 -0300

    Now meteor connections begins on app init, not in splash

--- app/index.ios.js
+++ app/index.ios.js
@@ -2,7 +2,10 @@
  * Federeichon Project App
  * https://github.com/joadr/federeichon
  */
+import Meteor from 'react-native-meteor'
 import PanelLayout from './js/layouts/panel-layout'
 import { AppRegistry } from 'react-native'
+
+Meteor.connect('ws://45.55.141.9/websocket')
 
 AppRegistry.registerComponent('Federeichon', () => PanelLayout)
