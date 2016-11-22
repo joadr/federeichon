eaf9966b6647270e3199297edbdb3bf9107a839d
Joaquin Diaz <joaquin.diaz@ies.cl>
2016-11-22 13:46:14 -0300

    news added

--- app/js/pages/news.js
+++ app/js/pages/news.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Icon } from 'native-base'
-import { Text, StyleSheet } from 'react-native'
+import { Container, Header, Title, Content, Button, Icon, Card, CardItem, Text } from 'native-base'
+import { StyleSheet } from 'react-native'
+import Meteor, { createContainer } from 'react-native-meteor'
 import theme from '../theme/federeichon-theme'
+import prune from 'underscore.string/prune'
 
 const propTypes = {
 
 }
 
-export default class News extends React.Component {
+class News extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -16,6 +18,21 @@ export default class News extends React.Component {
 
   componentDidMount () {
     this.props.acceptPan()
+  }
+
+  renderNews () {
+    return this.props.news.map((noticia, index) => {
+      return (
+        <Card key={index}>
+          <CardItem header onPress={() => { this.props.navigator.push({ name: 'singleNew', id: noticia._id }) }}>
+            <Text style={styles.title}>{noticia.title}</Text>
+          </CardItem>
+          <CardItem onPress={() => { this.props.navigator.push({ name: 'singleNew', id: noticia._id }) }}>
+            <Text>{prune(noticia.body, 200)}</Text>
+          </CardItem>
+        </Card>
+      )
+    })
   }
 
   render () {
@@ -28,16 +45,30 @@ export default class News extends React.Component {
           <Title>Noticias</Title>
         </Header>
         <Content>
-          <Text>Holaa csm3!!</Text>
+          {this.renderNews()}
         </Content>
       </Container>
     )
   }
 }
 
+export default createContainer(() => {
+  const handle = Meteor.subscribe('news-index')
+  const news = Meteor.collection('news').find()
+
+  return {
+    newsReady: handle.ready(),
+    news: news
+  }
+}, News)
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
   }
 })
 
